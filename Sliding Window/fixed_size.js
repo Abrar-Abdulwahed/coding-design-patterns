d1cc22e7cc50f911f6dd4d23aa0114d555a9cb64
Abrar-Abdulwahed <abrar.abdulwahed@gmail.com>
2023-11-20 23:36:17 +0300

    feat: sliding window with fixed size

---
+++ Sliding Window/fixed_size.js
@@ -0,0 +1,17 @@
+const maxSubArraySum = (arr, window_size) => {
+    if(window_size < 0 || window_size > arr.length === 0) return null;
+
+    let currSum = 0;
+    let maxSum = -Infinity;
+    arr.forEach((elem, i) => {
+        currSum += elem;
+        if(i >= (window_size - 1)){
+            maxSum = Math.max(currSum, maxSum);
+            currSum -= arr[i - (window_size - 1)]
+        }
+    });
+    return maxSum;
+}
+
+const arr = [1, 2, 10, 3, 4, 5, 6, 7, 10];
+console.log(maxSubArraySum(arr, 3));
